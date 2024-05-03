8d262c5cab2306068f364c58b05055157074a0ba
hamzasysh <157980968+hamzasysh@users.noreply.github.com>
2024-05-03 16:30:14 +0500

    UPDATE

--- myproject/settings.py
+++ myproject/settings.py
@@ -132,3 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 UNAME=os.getenv('USERNAME')
 PSWRD=os.getenv('PASSWORD')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [BASE_DIR / "static"]
